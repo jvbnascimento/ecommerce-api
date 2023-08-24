d32bc6ea6637be93e57d89c86f1cd883b912f1a9
jvbnascimento <joao.vitoor.batista@gmail.com>
2023-08-24 19:33:33 -0300

    Add new routes API: Admin and StoreFront; Concern authenticable created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sing_up, keys: [:name, :email, :password, :password_confirmation])
+    end
 end
